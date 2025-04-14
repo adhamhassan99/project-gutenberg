c4657fb509b06f4e1a5c3fe0e0692735ff7607f5
Adham <adhamhassan100@yahoo.com>
2025-04-15 01:14:10 +0200

    bypass cors issue

--- src/hooks/useFetchBookText.ts
+++ src/hooks/useFetchBookText.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const useFetchBookText = () => {
     return useMutation({
         mutationFn: async (id: string) => {
-            const res = await axios.get(`https://www.gutenberg.org/files/${id}/${id}.txt`)
+            const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://www.gutenberg.org/files/${id}/${id}.txt`)
             return res.data
         },
 
